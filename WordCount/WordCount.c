1100353726786feb09a0249ff3fb20abf6b6ac53
SailVen927 <zyf5135118@163.com>
2021-05-04 11:52:54 +0800

    增加读取固定文件，修复多个分隔符时wordCount的输出错误

--- WordCount/WordCount.c
+++ WordCount/WordCount.c
@@ -6,19 +6,35 @@ int main()
 	int charCount=0;//字符计数
 	int wordCount=0;//单词计数
 	int i=0;
+	int flag=0;//判断空格和逗号前后是否为同样的分隔符
 	char c;
 	//获取输入文本
 	char text[400]={""};
-	gets(text);
+	FILE* fp = fopen("context.txt", "r");
+    if(!fp) {
+        perror("File opening failed");
+        return 0;
+    }
+	
+    while ((c = fgetc(fp)) != EOF) { 
+	   text[i++]=c;
+    }
 	//计算
 	for(i=0;(c=text[i])!=NULL;i++)
 	{
-		if(c==',' || c==' ')
+		if(flag==0 && (c==',' || c==' '))//字母后首次遇到分隔符
 		{
+			flag=1;
+			continue;
+		}
+		if(flag==1 && !(c==',' || c==' '))//分隔符后遇到第一个字母
+		{
+			flag=0;
 			wordCount++;
 		}
 	}
 	charCount=i;
-	printf("%d %d",charCount,wordCount+1);
+	printf("%d %d\n",charCount,wordCount+1);
+	printf("%s",text);
 	return 0;
 }
